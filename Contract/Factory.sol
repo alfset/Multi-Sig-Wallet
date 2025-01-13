d4a2b5d499a314c2045c32359bad72b7649375b0
Alf <42708476+alfset@users.noreply.github.com>
2025-01-13 22:34:19 +0700

    Create Factory.sol

---
+++ Contract/Factory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./MultiSigWallet.sol";  // Assuming you have the MultiSigWallet contract
+
+contract MultiSigWalletFactory {
+    address[] public deployedWallets;
+    
+    event WalletCreated(address indexed walletAddress, address[] owners, uint requiredSignatures, address tokenAddress);
+
+    function createWallet(address[] memory _owners, uint _requiredSignatures, address _tokenAddress) public returns (address) {
+        MultiSigWallet newWallet = new MultiSigWallet(_owners, _requiredSignatures, _tokenAddress);
+        deployedWallets.push(address(newWallet));
+        
+        emit WalletCreated(address(newWallet), _owners, _requiredSignatures, _tokenAddress);
+        return address(newWallet);
+    }
+
+    function getDeployedWallets() public view returns (address[] memory) {
+        return deployedWallets;
+    }
+}
