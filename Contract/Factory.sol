282349528a0248671a790f2220bc07364280c2ca
Alf <42708476+alfset@users.noreply.github.com>
2025-01-24 23:44:46 -0500

    Update Factory.sol

--- Contract/Factory.sol
+++ Contract/Factory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./MultiSigWallet.sol";  // Assuming you have the MultiSigWallet contract
+import "./MultiSigWallet.sol";  
 
 contract MultiSigWalletFactory {
     address[] public deployedWallets;
